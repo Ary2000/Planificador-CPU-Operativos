0f09eede2aca8b7cf2d5758eedc70f701cf1e794
Isaac <irsacfac@gmail.com>
2022-04-07 01:39:19 -0600

    Cambios en planificador y FIFO
    FIFO parece funcionar, pero aun ocupa un par de retoques

---
+++ FIFO.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <unistd.h>
+#include "planificador.c"
+
+void *run(bool* activo){
+    while(activo){
+        printf("En el while\n");
+        if(largoLista() == 0){
+            sleep(1);
+            activo = false;
+        }else{
+            nodo* first = getPrimero();
+            //printf("%i  ,", first->info[0]);
+            sleep(first->info[0]);
+            printf("Vuelta\n");
+            eliminar(0);
+        }
+    }
+}
+
+int main(){
+    char datos[] = "2,7";
+    agregar(datos);
+    char datos2[] = "3,1";
+    mostrarLista();
+    agregar(datos2);
+    bool *activo = true;
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, run(activo), NULL);
+    if(activo){
+        printf("Thread prueba\n");
+    }
+    pthread_join(thread_id, NULL);
+    return 0;
+}
--- planificador.c
+++ planificador.c
@@ -10,9 +10,7 @@
 
 typedef struct{
    struct nodo* siguiente;
-   char* nombre;
-   int burst;
-   int prioridad;
+   int info[2];
 
 }nodo;
 
@@ -22,23 +20,21 @@ nodo* ultimo = NULL;
 void agregar(char *datos){
    nodo* dato = malloc(sizeof(nodo)); //Inicializa el nodo
    dato->siguiente = NULL;
+   dato->info[0] = 0;
+   dato->info[1] = 0;
    char *separador = ",";
    char *token = strtok(datos,separador);
    if(token != NULL){
-      dato->burst = atoi(token);
+      dato->info[0] = atoi(token);
       while (token != NULL)
       {
          token = strtok(NULL,separador);
-         //el error esta aqui, el token es nulo cuando no deberia serlo
          if(token != NULL){
-            dato->prioridad = atoi(token);
+            dato->info[1] = atoi(token);
          }else{
-            dato->prioridad = 0;
          }
       }
    }else{
-         dato->burst = 0;
-         dato->prioridad = 0;
    }
 
    if(primero == NULL){
@@ -61,11 +57,45 @@ int largoLista(){
    return cont;
 }
 
+nodo* getPrimero(){
+   return primero;
+}
+
+nodo* getUltimo(){
+   return ultimo;
+}
+
+nodo* getElemento(int pos){
+   /*if(pos == 0){
+      return getPrimero;
+   }*/
+   nodo* aux = primero;
+   int cont = 0;
+   while (aux != NULL)
+   {
+      if(cont == pos){
+         return aux;
+      }else{
+         aux = aux->siguiente;
+      }
+      cont = cont + 1;
+   }
+   return NULL;
+}
+
 void eliminar(int pos){
+   if(primero == NULL){
+      return;
+   }
    nodo* aux = primero;
    if(pos==0){
-      primero = primero->siguiente;
-      aux->siguiente = NULL;
+      if(primero->siguiente == NULL){
+         ultimo = NULL;
+         primero = NULL;
+      }else{
+         primero = primero->siguiente;
+         aux->siguiente = NULL;
+      }
       free(aux);
    }
    else{
@@ -73,55 +103,75 @@ void eliminar(int pos){
       for (int i = 0; i < largoLista(); ++i)
       {
          if(i==pos){
-            //nodo* aeliminar = aux->siguiente;
             if(pos == largoLista()-1){
                prev->siguiente = NULL;
                ultimo = prev;
-               //aux->siguiente = NULL;
             }
             else{
                prev->siguiente = aux->siguiente;
             }
-            int cont=1;
-            /*while(cont<pos){
-             aux = aux->siguiente;
-             cont++;
-            }*/
             aux->siguiente = NULL;
             free(aux);
-            //aux = aux->siguiente;
-            //aux->siguiente = aux->siguiente;
          }
          prev = aux;
          aux = aux->siguiente;
       }
 
    }
-   
 }
 
 void mostrarLista(){
    nodo* i = primero;
    while(i != NULL){
-      printf("%i  ,", i->burst);
-      printf("%i  \n", i->prioridad);
+      printf("%i  ,", i->info[0]);
+      printf("%i  \n", i->info[1]);
       i = i->siguiente;
    }
 
 }
 
+void liberarLista(){
+   if(primero == NULL){
+      return;
+   }
+   nodo* aux = primero;
+   primero = NULL;
+   while (aux != ultimo)
+   {
+      nodo* prev = aux;
+      aux = aux->siguiente;
+      prev->siguiente = NULL;
+      free(prev);
+   }
+   ultimo = NULL;
+}
 
-int main() {
+
+/*int main() {
+   printf("Largo de la lista: %d\n", largoLista());
    agregar("1");
+   printf("Largo de la lista: %d\n", largoLista());
    mostrarLista();
+   // si se quiere agregar datos separados por coma, por algun motivo tiene que ser mediante un array
    char datos[] = "2,7";
    agregar(datos);
+   printf("Largo de la lista: %d\n", largoLista());
    mostrarLista();
-   //agregar("3,1");
-   /*mostrarLista();
+   char datos2[] = "3,1";
+   agregar(datos2);
+   mostrarLista();
    printf("Largo de la lista: %d\n", largoLista());
-   eliminar(1);
+   eliminar(0);
+   eliminar(0);
    printf("Largo de la lista: %d\n", largoLista());
-   mostrarLista();*/
+   mostrarLista();
+   nodo *prueba = getElemento(0);
+   printf("Prueba de getElemento: %d\n", prueba->info[0]);
+   prueba = getUltimo();
+   printf("Prueba de getElemento: %d\n", prueba->info[0]);
+   prueba = getPrimero();
+   printf("Prueba de getElemento: %d\n", prueba->info[0]);
+   free(prueba);
+   liberarLista();
 	return 0;
-}
+}*/
